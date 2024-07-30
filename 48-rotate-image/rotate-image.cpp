31145af9ba896d419b257172cbe7cd36612623dc
Sahil Lathiya <68152820+SahilLathiya@users.noreply.github.com>
2024-07-30 18:44:46 +0530

    Time: 4 ms (38.54%) | Memory: 8.9 MB (27.05%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-
     void transpose(vector<vector<int>>& matrix){
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[0].size();j++){
-                if(i<j){
-                    swap(matrix[i][j],matrix[j][i]);
-                }
+        int m = matrix.size();
+        for(int i=0; i<m; i++){
+            for(int j=0; j<i; j++){
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+    }
+    void swapCol(vector<vector<int>>& matrix){
+        int m = matrix.size();
+        for(int i=0;i<m/2;i++){
+            for(int j=0;j<m;j++){
+                swap(matrix[j][i], matrix[j][m-1-i]);
             }
         }
     }
     void rotate(vector<vector<int>>& matrix) {
         transpose(matrix);
-
-        int s_col=0, e_col=matrix[0].size()-1;
-        while(s_col<e_col){
-            for(int i=0;i<matrix.size();i++){
-                swap(matrix[i][s_col],matrix[i][e_col]);
-            }
-            s_col++;
-            e_col--;
-        }
+        swapCol(matrix);
     }
 };
