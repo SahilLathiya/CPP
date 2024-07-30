8d51c4e86ce34a520e2ee6e25f9ec44dbf759a4f
Sahil Lathiya <68152820+SahilLathiya@users.noreply.github.com>
2024-07-30 19:18:32 +0530

    Time: 9 ms (14.81%) | Memory: 14.8 MB (16.44%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void reverse(vector<int>& nums, int low, int high){
         while(low<high){
-            swap(nums[low],nums[high]);
+            swap(nums[low], nums[high]);
             low++;
             high--;
         }
@@ -10,8 +10,8 @@ public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
         int ind = -1;
-        for(int i = n-2;i>=0;i--){
-            if(nums[i+1]>nums[i]){
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
                 ind = i;
                 break;
             }
@@ -20,10 +20,9 @@ public:
             reverse(nums,0,n-1);
             return;
         }
-        
-        for(int i=n-1;i>ind;i--){
+        for(int i = n-1;i>ind;i--){
             if(nums[i]>nums[ind]){
-                swap(nums[i],nums[ind]);
+                swap(nums[i], nums[ind]);
                 break;
             }
         }
