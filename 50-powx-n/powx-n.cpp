32b7f066dbdffcbc37854385cab9e08b35ae4fd4
Sahil Lathiya <68152820+SahilLathiya@users.noreply.github.com>
2024-07-31 17:41:53 +0530

    Time: 4 ms (21.54%) | Memory: 8.4 MB (6.52%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -2,17 +2,21 @@ class Solution {
 public:
     double myPow(double x, int n) {
         if(n==0)
-            return 1;
-        if(n>0){
+            return 1.0;
+        else if(n>0){
             double t = myPow(x,n/2);
-            if(n%2)
+            if(n%2==0)
+                return t*t;
+            else
                 return x*t*t;
-            return t*t;
         }
-            
-        double t = myPow(x,n/2);
-        if(n%2)
-            return (1/x)*t*t;
-        return t*t;
+        else{
+            double t = myPow(x,n/2);
+            if(n%2==0)
+                return t*t;
+            else
+                return (1/x)*t*t;
+        }
+        return -1;
     }
 };
