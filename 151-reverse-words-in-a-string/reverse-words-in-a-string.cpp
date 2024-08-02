361d98bff8b8ea75e856f9b82027ef6620328e90
Sahil Lathiya <68152820+SahilLathiya@users.noreply.github.com>
2024-08-02 17:15:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (74.41%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,45 +1,40 @@
-#include <iostream>
-#include <string>
-#include <sstream>
-
 class Solution {
 public:
-void reverse(string &s, int l, int h){
-    while(l<h){
-        swap(s[l],s[h]);
-        l++;
-        h--;
-    }
-}
-std::string removeExtraSpaces(const std::string& input) {
-    std::istringstream iss(input);
-    std::string result, word;
-
-    while (iss >> word) {
-        result += word + " ";
-    }
-
-    // Remove trailing space
-    if (!result.empty()) {
-        result.pop_back();
-    }
-
-    return result;
-}
-    string reverseWords(string s) {
-        int n = s.size();
-        reverse(s,0,n-1);
-        int l = 0,i=0;
-        while(i<n){
-            if(s[i]==' '){
-                reverse(s,l,i-1);
-                l = i+1;
-            }
-            i++;
+    void reverse(string* s, int l, int h){
+        while(l<h){
+            swap((*s)[l],(*s)[h]);
+            h--;
+            l++;
         }
-        reverse(s,l,n-1);
-        s = removeExtraSpaces(s);
-        
-        return s;
+    }
+    string reverseWords(string s) {
+        int l = s.length();
+        reverse(&s,0,l-1);
+        // return s;
+
+        int first, last;
+        for(int i=0;i<l;i++){
+            if(s[i]==' ')
+                continue;
+            first = i;
+            while(i<l && s[i]!=' ')
+                i++;
+            last = i;
+            reverse(&s, first, last-1);
+        }
+
+        string s2;
+        bool flag = false;
+        for(int i=0;i<l;i++){
+            while(i<l && s[i]==' ')
+                i++;
+            if(i==l)
+                break;
+            if(flag && s[i-1]==' ')
+                s2.push_back(' ');
+            s2.push_back(s[i]);
+            flag = true;
+        }
+        return s2;
     }
 };
