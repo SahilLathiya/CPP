f4a0b013326edaece22e96f6c15c72133fdd17e1
Sahil Lathiya <68152820+SahilLathiya@users.noreply.github.com>
2024-08-08 21:44:31 +0530

    Time: 88 ms (8.40%) | Memory: 41.2 MB (19.20%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void subset(vector<int> &arr, int ind, int n, vector<int> temp, vector<vector<int>> &ans, int sum, int target){
+        if(sum==target){
+            ans.push_back(vector<int> (temp));
+            return;
+        }
+        if(ind>=n || sum>target)
+            return;
+
+        subset(arr, ind+1, n, temp, ans, sum, target);
+
+        while(sum<=target){
+            sum+=arr[ind];
+            temp.push_back(arr[ind]);
+            subset(arr, ind+1, n, temp, ans, sum, target);
+            // temp.pop_back();
+        }
+
+        // subset(arr, ind+1, n, temp, ans, sum, target);
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& arr, int target) {
+        sort(arr.begin(),arr.end());
+        vector<vector<int>> ans;
+        vector<int> temp;
+        subset(arr, 0, arr.size(), temp, ans, 0, target);
+        
+        return ans;
+    }
+};
