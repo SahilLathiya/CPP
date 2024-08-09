03fae2341e01a4f364ef0e883a5ed33b52d6b7d1
Sahil Lathiya <68152820+SahilLathiya@users.noreply.github.com>
2024-08-09 22:35:53 +0530

    Time: 4 ms (70.18%) | Memory: 12.5 MB (5.27%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -2,36 +2,33 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& arr1, vector<int>& arr2) {
         stack<int> st;
+        unordered_map<int,int> mpp;
         int n = arr1.size();
         int m = arr2.size();
-        vector<int> ans(m);
+
         for(int i=m-1;i>=0;i--){
             if(st.size()==0 || st.top()>arr2[i]){
                 if(st.size()==0)
-                    ans[i] = -1;
+                    mpp[arr2[i]] = -1;
                 else
-                    ans[i] = st.top();
+                    mpp[arr2[i]] = st.top();
                 st.push(arr2[i]);
             }
             else {
                 while(st.size()>0 && st.top()<arr2[i])
                     st.pop();
                 if(st.size()==0)
-                    ans[i] = -1;
+                    mpp[arr2[i]] = -1;
                 else
-                    ans[i] = st.top();
+                    mpp[arr2[i]] = st.top();
                 st.push(arr2[i]);
             }
         }
-        vector<int> ans2;
+
+        vector<int> ans;
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(arr1[i]==arr2[j]){
-                    ans2.push_back(ans[j]);
-                    break;
-                }
-            }
+            ans.push_back(mpp[arr1[i]]);
         }
-        return ans2;
+        return ans;
     }
 };
