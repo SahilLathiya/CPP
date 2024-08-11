b3bc7d7cf6d46b99f5030d20c77a31ba589f2af2
Sahil Lathiya <68152820+SahilLathiya@users.noreply.github.com>
2024-08-11 14:51:00 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (6.61%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -15,7 +15,14 @@ public:
         return dp[n] = l+r;
     }
     int climbStairs(int n) {
+        if(n<=2)
+            return n;
         vector<int> dp(n+1, -1);
-        return solve(n, dp);
+        // return solve(n, dp);
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i=3;i<=n;i++)
+            dp[i] = dp[i-1] + dp[i-2];
+        return dp[n];
     }
 };
