0b9e20c996978b8592f9b08e1bf7a11b36089542
Sahil Lathiya <68152820+SahilLathiya@users.noreply.github.com>
2024-08-12 22:28:53 +0530

    Time: 11 ms (46.75%) | Memory: 16.7 MB (55.13%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& arr) {
-        int col0 = arr[0][0];   
-        int m = arr.size(), n=arr[0].size();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        int col0 = arr[0][0];
+        int n = arr.size();
+        int m = arr[0].size();
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(arr[i][j]==0){
                     if(j==0)
                         col0 = 0;
                     else
                         arr[0][j] = 0;
-
                     arr[i][0] = 0;
+
                 }
             }
         }
-
-        for(int i = arr.size()-1;i>0;i--){
-            for(int j = arr[0].size()-1;j>0;j--){
+        
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
                 if(arr[i][0]==0 || arr[0][j]==0)
                     arr[i][j] = 0;
             }
         }
-        
+
         if(arr[0][0]==0)
-            for(int i=0;i<n;i++) arr[0][i] = 0;
+            for(int i=0;i<m;i++)
+                arr[0][i] = 0;
         if(col0==0)
-            for(int i=0;i<m;i++) arr[i][0] = 0;
+            for(int i=0;i<n;i++)
+                arr[i][0] = 0;
     }
 };
