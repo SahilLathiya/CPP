709cf09f85f6483e94c493becc0e7b449c001932
Sahil Lathiya <68152820+SahilLathiya@users.noreply.github.com>
2024-08-14 11:37:08 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (12.78%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -17,6 +17,19 @@ public:
         for(int i=0;i<m;i++)
             for(int j=0;j<n;j++)
                 dp[i][j] = -1;
-        return solve(m-1, n-1, dp);
+        // return solve(m-1, n-1, dp);
+
+        for(int i=0;i<m;i++)
+            dp[i][0] = 1;
+        
+        for(int i=0;i<n;i++)
+            dp[0][i] = 1;
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
