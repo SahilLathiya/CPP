b59bd4b2547c4d2df09bbf2252ec03cb1b45d314
Sahil Lathiya <68152820+SahilLathiya@users.noreply.github.com>
2024-08-14 12:19:39 +0530

    Time: 3 ms (34.23%) | Memory: 7.7 MB (62.09%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -13,23 +13,27 @@ public:
         return dp[m][n] = l+r;
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int> (n));
-        for(int i=0;i<m;i++)
-            for(int j=0;j<n;j++)
-                dp[i][j] = -1;
+        // vector<vector<int>> dp(m, vector<int> (n));
+        // for(int i=0;i<m;i++)
+        //     for(int j=0;j<n;j++)
+        //         dp[i][j] = -1;
         // return solve(m-1, n-1, dp);
 
-        for(int i=0;i<m;i++)
-            dp[i][0] = 1;
+        // for(int i=0;i<m;i++)
+        //     dp[i][0] = 1;
         
-        for(int i=0;i<n;i++)
-            dp[0][i] = 1;
+        // for(int i=0;i<n;i++)
+        //     dp[0][i] = 1;
 
-        for(int i=1;i<m;i++){
+        vector<int> prev(n, 0);
+        for(int i=0;i<m;i++){
+            vector<int> temp(n);
+            temp[0] = 1;
             for(int j=1;j<n;j++){
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                temp[j] = prev[j] + temp[j-1];
             }
+            prev = temp;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
