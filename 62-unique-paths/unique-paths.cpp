d513502d5876f4ada371c2e85b750f41001523fc
Sahil Lathiya <68152820+SahilLathiya@users.noreply.github.com>
2024-08-14 20:10:39 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (72.96%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
-    int solve(int m, int n, vector<vector<int>> &dp){
+    int solve(int m,int n, vector<vector<int>> &dp){
         if(m<0 || n<0)
             return 0;
-        if(m==0 && n==0){
+        if(m==0 && n==0)
             return 1;
-        }
+
         if(dp[m][n]!=-1)
             return dp[m][n];
-        int l = solve(m-1,n, dp);
-        int r = solve(m,n-1, dp);
+
+        int l = solve(m-1, n, dp);
+        int r = solve(m, n-1, dp);
         return dp[m][n] = l+r;
     }
     int uniquePaths(int m, int n) {
-        // vector<vector<int>> dp(m, vector<int> (n));
+        // vector<vector<int>> dp(m, vector<int>(n));
         // for(int i=0;i<m;i++)
         //     for(int j=0;j<n;j++)
         //         dp[i][j] = -1;
+
         // return solve(m-1, n-1, dp);
 
-        // for(int i=0;i<m;i++)
-        //     dp[i][0] = 1;
-        
-        // for(int i=0;i<n;i++)
-        //     dp[0][i] = 1;
-
-        vector<int> prev(n, 0);
+        vector<int> temp(n);
+        vector<int> prev(n);
+        temp[0] = 1;
         for(int i=0;i<m;i++){
-            vector<int> temp(n);
-            temp[0] = 1;
-            for(int j=1;j<n;j++){
-                temp[j] = prev[j] + temp[j-1];
+            for(int j=0;j<n;j++){
+                if(i==0&&j==0)
+                    continue;
+                int l = 0, r = 0;
+                if(i>0) l = prev[j];
+                if(j>0) r = temp[j-1];
+                temp[j] = l+r;
             }
             prev = temp;
         }
