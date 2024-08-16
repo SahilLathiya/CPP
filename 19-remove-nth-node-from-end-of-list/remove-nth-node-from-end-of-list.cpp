559e9e76e0001d176c199b61c5052d68c24d5302
Sahil Lathiya <68152820+SahilLathiya@users.noreply.github.com>
2024-08-16 13:20:51 +0530

    Time: 3 ms (63.40%) | Memory: 14.8 MB (21.90%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -10,49 +10,26 @@
  */
 class Solution {
 public:
-    int lengthOfLL(ListNode* head){
-        int cnt = 0;
-        while(head){
-            cnt++;
-            head = head->next;
-        }
-        return cnt;
-    }
-    ListNode* removeNthNode(ListNode* head, int n){
-        if(head==NULL)
-            return head;
-
-        ListNode* ptr = head;
-        ListNode* prev = NULL;
-        int cnt = 0;
-        while(cnt<n){
-            cnt++;
-            prev = ptr;
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode * ptr = head;
+        int len = 0;
+        while(ptr){
+            len++;
             ptr = ptr->next;
         }
-        if(prev){
-            cout<<ptr->val<<' ';
-            prev->next = ptr->next;
-        }
-        else{
-            // cout<<"TTT";
-            // ptr = head;
-            // while(ptr->next!=NULL){
-            //     prev = ptr;
-            //     ptr = ptr->next;
-            //     cout<<"werwe";
-            // }
-            // prev->next = NULL;
-            return head->next;
-        }
 
-        return head;
-    }
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
-        int len = lengthOfLL(head);
-        if(len==1 && n>=1)
-            return NULL;
-        head = removeNthNode(head, len - n);
+        
+        int t = len - (n-1);
+        if(t==1)
+            return head->next;
+        ListNode * temp = head;
+        ListNode * prev = NULL;
+        while(--t){
+            prev = temp;
+            temp = temp->next;
+        }
+        prev->next = temp->next;
+        delete temp;
         return head;
     }
 };
