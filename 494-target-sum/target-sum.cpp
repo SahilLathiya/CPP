b8c4c7a13bc97508441a0496f8ce15033d60f270
Sahil Lathiya <68152820+SahilLathiya@users.noreply.github.com>
2024-08-18 16:01:08 +0530

    Time: 44 ms (41.73%) | Memory: 317.1 MB (5.31%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int solve(vector<int> &arr, int ind, int target){
+    int solve(vector<int> &arr, int ind, int target, vector<vector<int>> &dp){
+        // cout<<ind<<' ';
         if(ind==0){
             if(arr[0]==0 && target==0)
                 return 2;
@@ -8,15 +9,22 @@ public:
                 return 1;
             return 0;
         }
+        // cout<<20000-target<<' ';
+        if(dp[ind][20000-target]!=-1)
+            return dp[ind][20000-target];
+
         //take as it is
-        int l = solve(arr, ind-1, target-arr[ind]);
+        int l = solve(arr, ind-1, target-arr[ind], dp);
 
         // take negative
-        int r = solve(arr, ind-1, target+arr[ind]);
-        return l+r;
+        int r = solve(arr, ind-1, target+arr[ind], dp);
+        return dp[ind][20000-target] = l+r;
     }
     int findTargetSumWays(vector<int>& arr, int target) {
-        
-        return solve(arr, arr.size()-1, target);
+        int n = arr.size();
+
+        vector<vector<int>> dp(n, vector<int> (40000+1, -1));
+        return solve(arr, n-1, target, dp);
+        return 1;
     }
 };
