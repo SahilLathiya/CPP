012218620a762f0169f6a28aed748c250e37e581
Sahil Lathiya <68152820+SahilLathiya@users.noreply.github.com>
2024-08-19 19:48:45 +0530

    Time: 38 ms (27.39%) | Memory: 26.1 MB (23.01%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solve(string s1, string s2, int ind1, int ind2, vector<vector<int>> &dp){
+    int solve(string &s1, string &s2, int ind1, int ind2, vector<vector<int>> &dp){
         if(ind1<0 || ind2<0)
             return 0;
         if(dp[ind1][ind2]!=-1)
@@ -20,32 +20,32 @@ public:
     int longestCommonSubsequence(string s1, string s2) {
         int n1 = s1.length();
         int n2 = s2.length();
-        // vector<vector<int>> dp(n1, vector<int> (n2, -1));
+        vector<vector<int>> dp(n1, vector<int> (n2, -1));
 
-        // return solve(s1, s2, n1-1, n2-1, dp);
+        return solve(s1, s2, n1-1, n2-1, dp);
 
 
-        vector<int> temp(n2, 0), prev(n2, 0);
-        for(int i=0;i<n1;i++){
-            for(int j=0;j<n2;j++){
-                //match
-                int l = 0;
-                int r1 = 0, r2 = 0;
-                if(i>0 && j>0 && s1[i]==s2[j])
-                    l = 1 + prev[j-1];
-                else if(s1[i]==s2[j])
-                    l = 1;
-                else{
-                    if(i>0)
-                        r1 = prev[j];
-                    if(j>0)
-                        r2 = temp[j-1];
-                }
-                temp[j] = max(l, max(r1, r2));
-            }
-            prev = temp;
-        }
-        return prev[n2-1];
+        // vector<int> temp(n2, 0), prev(n2, 0);
+        // for(int i=0;i<n1;i++){
+        //     for(int j=0;j<n2;j++){
+        //         //match
+        //         int l = 0;
+        //         int r1 = 0, r2 = 0;
+        //         if(i>0 && j>0 && s1[i]==s2[j])
+        //             l = 1 + prev[j-1];
+        //         else if(s1[i]==s2[j])
+        //             l = 1;
+        //         else{
+        //             if(i>0)
+        //                 r1 = prev[j];
+        //             if(j>0)
+        //                 r2 = temp[j-1];
+        //         }
+        //         temp[j] = max(l, max(r1, r2));
+        //     }
+        //     prev = temp;
+        // }
+        // return prev[n2-1];
         
     }
 };
