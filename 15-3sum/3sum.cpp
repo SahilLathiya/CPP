9890c16b2b21503bf4b7cd75ce25aa2c297880b2
Sahil Lathiya <68152820+SahilLathiya@users.noreply.github.com>
2024-08-20 14:55:25 +0530

    Time: 90 ms (30.21%) | Memory: 29.4 MB (42.68%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,40 +1,43 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& arr) {
+        int n = arr.size();
+        int x = 0;
         vector<vector<int>> ans;
-        int n= arr.size();
         sort(arr.begin(), arr.end());
         for(int i=0;i<n;i++){
             if(i>0 && arr[i-1]==arr[i])
                 continue;
-            int sum = arr[i];
-            int j=i+1, k=n-1;
+            int j = i+1, k = n-1;
             while(j<k){
-                
-                if(sum + arr[j]+arr[k] <0){
+                if(j>i+1 && arr[j-1]==arr[j]){
                     j++;
+                    continue;
                 }
-                else if(sum + arr[j]+arr[k] >0){
+                if(k<n-1 && arr[k]==arr[k+1]){
                     k--;
+                    continue;
                 }
-                else{
+
+                int sum = arr[i]+arr[j]+arr[k];
+                // cout<<arr[i]<<' '<<arr[j]<<' '<<arr[k]<<' '<<sum<<endl;
+                if(sum==x){
                     vector<int> t(3);
                     t[0] = arr[i];
                     t[1] = arr[j];
                     t[2] = arr[k];
                     ans.push_back(t);
-                    do{
-                        j++;
-                    }while(j<k && arr[j-1]==arr[j]);
-                    
-                    do{
-                        k--;
-                    }while(j<k && arr[k+1]==arr[k]);
-                        
+                    j++;
+                    k--;
+                }
+                else if(sum<x){
+                    j++;
+                }
+                else{
+                    k--;
                 }
             }
         }
-
         return ans;
     }
 };
