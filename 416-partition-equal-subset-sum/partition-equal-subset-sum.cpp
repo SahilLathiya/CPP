cee2b02915568af7646fa5b717024c29c85e4dca
Sahil Lathiya <68152820+SahilLathiya@users.noreply.github.com>
2024-08-21 00:48:43 +0530

    Time: 131 ms (69.37%) | Memory: 98.9 MB (36.58%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,52 +1,21 @@
 class Solution {
 public:
-    bool solve(vector<int> &arr, int ind, int k, vector<vector<int>> &dp){
-        if(k==0)
+    bool solve(vector<int>& arr, int ind, int sum, vector<vector<int>> &dp){
+        if(sum==0)
             return true;
-        if(ind<0 || k<0)
+        if(ind<0 || sum<0)
             return false;
-        if(dp[ind][k]!=-1)
-            return dp[ind][k];
             
-        //Not take
-        bool l = solve(arr, ind-1, k, dp);
-        
+        if(dp[ind][sum]!=-1)
+            return dp[ind][sum];
+        // cout<<ind<<' '<<sum<<endl;
         //take
-        bool r = solve(arr, ind-1, k-arr[ind], dp);
-        return dp[ind][k] = (l || r);
-    }
-    bool subsetSumToK(int n, int k, vector<int> &arr) {
-        // Write your code here.
-        // vector<vector<int>> dp(n, vector<int> (k+1, -1));
-        // return solve(arr, n-1, k, dp);
-
-        // vector<vector<int>> dp(n, vector<int> (k+1, 0));
-
-
-        // for(int i=0;i<n;i++)
-        //     dp[i][0] = true;
-
-        vector<bool> prev(k+1, 0), temp(k+1, 0);
-        temp[0] = true;
-        prev[0] = true;
-        if(arr[0]<=k)
-            prev[arr[0]] = true;
-
-        for(int i=1;i<n;i++){
-            for(int t=1;t<=k;t++){
-                //Not take
-                bool l = prev[t];
-            
-                //take
-                bool r = false;
-                if(t-arr[i]>=0)
-                    r = prev[t-arr[i]];
-                temp[t] = (l || r);
-            }
-            prev = temp;
-        }
-
-        return prev[k];
+        bool l = false;
+        l = solve(arr, ind-1, sum-arr[ind], dp);
+        //not take
+        bool r = solve(arr, ind-1, sum, dp);
+        return dp[ind][sum] = l|r;
+        // return l|r;
     }
     bool canPartition(vector<int>& arr) {
         int sum = 0;
@@ -55,7 +24,7 @@ public:
             sum+=arr[i];
         if(sum%2)
             return false;
-
-        return subsetSumToK(n, sum/2, arr);
+        vector<vector<int>> dp(n, vector<int>((sum/2)+1, -1));
+        return solve(arr, n-1, sum/2, dp);
     }
 };
