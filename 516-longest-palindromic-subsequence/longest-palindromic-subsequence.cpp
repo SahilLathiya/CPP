d35e701c69cb2c6a5930ab1ef12e72aaab948d34
Sahil Lathiya <68152820+SahilLathiya@users.noreply.github.com>
2024-08-20 20:27:06 +0530

    Time: 136 ms (5.20%) | Memory: 74.6 MB (20.64%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -21,7 +21,24 @@ public:
         string s1 = s;
         reverse(s.begin(), s.end());
         int n = s.length();
-        vector<vector<int>> dp(n, vector<int> (n, -1));
-        return solve(s1, s, n-1, n-1, dp);
+        // vector<vector<int>> dp(n, vector<int> (n, -1));
+        // return solve(s1, s, n-1, n-1, dp);
+
+        vector<vector<int>> dp(n+1, vector<int> (n+1, 0));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                //match
+                int l = 0;
+                if(s1[i-1]==s[j-1])
+                    l = 1 + dp[i-1][j-1];
+
+                //not match
+                int r1 = 0, r2 = 0;
+                r1 = dp[i-1][j];
+                r2 = dp[i][j-1];
+                dp[i][j]=max(l, max(r1, r2));
+            }
+        }
+        return dp[n][n];
     }
 };
