54ebea2802a2d015343c985c38fce23e9fe5be24
Sahil Lathiya <68152820+SahilLathiya@users.noreply.github.com>
2024-08-21 20:38:59 +0530

    Time: 13 ms (81.11%) | Memory: 20.1 MB (61.26%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -6,8 +6,10 @@ public:
 
         if(ind1<0)
             return 0;
+            
         if(dp[ind1][ind2]!=-1)
             return dp[ind1][ind2];
+
         if(s[ind1]==t[ind2]){
             //take
             int l = solve(s, t, ind1-1, ind2-1, dp);
@@ -22,7 +24,27 @@ public:
     int numDistinct(string s, string t) {
         int n = s.length();
         int m = t.length();
-        vector<vector<int>> dp(n, vector<int> (m ,-1));
-        return solve(s, t, n-1, m-1, dp);
+        // vector<vector<int>> dp(n, vector<int> (m ,-1));
+        // return solve(s, t, n-1, m-1, dp);
+
+        vector<vector<int>> dp(n+1, vector<int> (m+1, 0));
+        for(int i=0;i<=n;i++)
+            dp[i][0] = 1;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1]){
+                    //take
+                    int l = dp[i-1][j-1];
+
+                    //not take
+                    int r = dp[i-1][j];
+                    dp[i][j] = (long long int)l+r;
+                }
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[n][m];
     }   
 };
