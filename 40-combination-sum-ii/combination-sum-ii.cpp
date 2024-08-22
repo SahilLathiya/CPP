7c2efc25480b8634a0a8668ab4e4e6ceed41cdf1
Sahil Lathiya <68152820+SahilLathiya@users.noreply.github.com>
2024-08-22 19:36:26 +0530

    Time: 9 ms (19.49%) | Memory: 12.9 MB (41.15%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,54 +1,24 @@
 class Solution {
-    private:
-    void backtrack(std::vector<int>& candidates, int target, int start, 
-                   std::vector<int>& combination, std::vector<std::vector<int>>& result) {
-        if (target == 0) {
-            result.push_back(combination);
-            return;
-        }
-        
-        if (target < 0) {
-            return;
-        }
-        
-        for (int i = start; i < candidates.size(); ++i) {
-            // Skip duplicates
-            if (i > start && candidates[i] == candidates[i - 1]) {
-                continue;
-            }
-            combination.push_back(candidates[i]);
-            backtrack(candidates, target - candidates[i], i + 1, combination, result);
-            combination.pop_back(); // Backtrack
-        }
-    }
-
 public:
-
-    void solve(vector<int>& arr, int ind, int n, vector<int> &temp, vector<vector<int>> &ans, int sum, int target){        
-        // cout<<sum<<' ';
-        if(sum==target){
+    void solve(vector<int>& arr, int ind, int n, vector<int>& temp, vector<vector<int>> &ans, int target){
+        if(target==0){
             ans.push_back(temp);
-            return;
         }
-        if(sum>target)
+        if(target<0)
             return;
-
         for(int i=ind;i<n;i++){
-            sum+=arr[i];
+            if(i!=ind && arr[i-1]==arr[i])
+                continue;
             temp.push_back(arr[i]);
-            solve(arr, i+1, n, temp, ans, sum, target);
+            solve(arr, i+1, n, temp, ans, target-arr[i]);
             temp.pop_back();
-            sum-=arr[i];
-            while(i<n-1 && arr[i]==arr[i+1])
-                i++;
         }
     }
-    std::vector<std::vector<int>> combinationSum2(std::vector<int>& candidates, int target) {
-        std::vector<std::vector<int>> result;
-        std::vector<int> combination;
-        std::sort(candidates.begin(), candidates.end()); // Sort to handle duplicates
-        backtrack(candidates, target, 0, combination, result);
-        return result;
+    vector<vector<int>> combinationSum2(vector<int>& arr, int target) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        sort(arr.begin(), arr.end());
+        solve(arr, 0, arr.size(), temp, ans, target);
+        return ans;
     }
-
 };
