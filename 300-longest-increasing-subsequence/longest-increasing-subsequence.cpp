f16b7291660b9a1fe7a17193d50003c45bb24a70
Sahil Lathiya <68152820+SahilLathiya@users.noreply.github.com>
2024-08-23 23:23:11 +0530

    Time: 460 ms (28.48%) | Memory: 330.2 MB (8.83%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -3,6 +3,7 @@ public:
     int solve(vector<int>& arr, int ind, int lastTaken, vector<vector<int>> &dp){
         if(ind<0)
             return 0;
+
         if(dp[ind][lastTaken]!=-1)
             return dp[ind][lastTaken];
 
@@ -27,7 +28,30 @@ public:
             arr[i] -=mini;
             maxi = max(maxi, arr[i]);
         }
-        vector<vector<int>> dp(n, vector<int>(maxi+2, -1));
-        return solve(arr, n-1, maxi+1, dp);
+        // cout<<maxi<<endl;
+        // vector<vector<int>> dp(n, vector<int>(maxi+2, -1));
+        // return solve(arr, n-1, maxi+1, dp);
+
+        vector<vector<int>> dp(n+1, vector<int>(maxi+2, 0));
+        for(int i = 0;i<n;i++){
+            for(int j = 0; j<=maxi+1;j++){
+                int l = 0;
+                //take
+                if(arr[i]<j){
+                    l = 1 + dp[i][arr[i]];
+                }
+                //not take
+                int r = dp[i][j];
+                dp[i+1][j] = max(l, r);
+                // cout<<dp[i+1][j]<<' ';
+            }
+            // cout<<endl;
+        }
+        // for (auto i : dp[n])
+        //     cout<<i<<' ';
+        // cout<<dp.size()<<' ';
+        // cout<<dp[0].size();
+        return dp[n][maxi+1];
+        // return 0;
     }
 };
