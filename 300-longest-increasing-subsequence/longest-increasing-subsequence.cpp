5cceb65128c679b66b544433a6d6f1bd8d2811ba
Sahil Lathiya <68152820+SahilLathiya@users.noreply.github.com>
2024-08-23 23:11:15 +0530

    Time: 382 ms (30.32%) | Memory: 330.2 MB (8.83%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,51 +1,33 @@
 class Solution {
 public:
-    int solve(vector<int> &arr, int ind, int lastEle, vector<vector<int>> &dp){
+    int solve(vector<int>& arr, int ind, int lastTaken, vector<vector<int>> &dp){
         if(ind<0)
             return 0;
-        if(dp[ind][lastEle]!=-1)
-            return dp[ind][lastEle];
+        if(dp[ind][lastTaken]!=-1)
+            return dp[ind][lastTaken];
 
-        //take
         int l = 0;
-        if(arr[ind]<lastEle)
+        //take
+        if(arr[ind]<lastTaken){
             l = 1 + solve(arr, ind-1, arr[ind], dp);
+        }
         //not take
-        int r = solve(arr, ind-1, lastEle, dp);
 
-        return dp[ind][lastEle] = max(l, r);
+        int r = solve(arr, ind-1, lastTaken, dp);
+        return dp[ind][lastTaken] = max(l, r);
     }
     int lengthOfLIS(vector<int>& arr) {
-        int mini = INT_MAX;
         int n = arr.size();
+        int mini = INT_MAX;
         for(int i=0;i<n;i++)
             mini = min(mini, arr[i]);
         
         int maxi = INT_MIN;
         for(int i=0;i<n;i++){
-            arr[i] -= mini;
-            // cout<<arr[i]<<' ';
+            arr[i] -=mini;
             maxi = max(maxi, arr[i]);
         }
-        // cout<<endl<<maxi;
-
-        // vector<vector<int>> dp(n, vector<int>(maxi+2, -1));
-        // return solve(arr, n-1, maxi+1, dp);
-
-
-        vector<int> prev(maxi+2, 0);
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<=maxi+1;j++){
-                //take
-                int l = 0;
-                if(arr[i]<j)
-                    l = 1 + prev[arr[i]];
-                //not take
-                int r = prev[j];
-                prev[j] = max(l, r);
-            }
-        }
-        return prev[maxi+1];
+        vector<vector<int>> dp(n, vector<int>(maxi+2, -1));
+        return solve(arr, n-1, maxi+1, dp);
     }
 };
