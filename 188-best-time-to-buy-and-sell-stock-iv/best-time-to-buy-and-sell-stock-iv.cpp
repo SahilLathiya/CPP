265a27539c66e6bd4c211246faeeb158fced53d1
Sahil Lathiya <68152820+SahilLathiya@users.noreply.github.com>
2024-08-23 16:59:43 +0530

    Time: 8 ms (69.20%) | Memory: 16.8 MB (38.26%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -3,6 +3,7 @@ public:
     int solve(vector<int>& arr, int ind, int n, bool canBuy, int remTrans, vector<vector<vector<int>>> &dp){
         if(ind==n || remTrans==0)
             return 0;
+
         if(dp[ind][canBuy][remTrans]!=-1)
             return dp[ind][canBuy][remTrans];
 
@@ -24,9 +25,44 @@ public:
         }
         return dp[ind][canBuy][remTrans] = profit;
     }
-    int maxProfit(int k, vector<int>& arr) {
+    int maxProfit(int k1, vector<int>& arr) {
         int n = arr.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>> (2, vector<int> (k+1, -1)));
-        return solve(arr, 0, n, 1, k, dp);
+        // vector<vector<vector<int>>> dp(n, vector<vector<int>> (2, vector<int> (3, -1)));
+        // return solve(arr, 0, n, 1, 2, dp);
+        
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>> (2, vector<int> (k1+1, 0)));
+        for(int i=0;i<2;i++){
+            for(int j=0;j<k1;j++)
+                dp[n][i][j] = 0;
+        } 
+        for(int i=0;i<n+1;i++){
+            dp[i][0][0] = 0;
+            dp[i][1][0] = 0;
+        }
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=1;j>=0;j--){
+                for(int k=k1;k>0;k--){
+                    int profit = 0;
+                    if(j){
+                        //buy
+                        int l = -arr[i] + dp[i+1][0][k];
+                        //not buy
+                        int r = dp[i+1][1][k];
+                        profit = max(l, r);
+                    }
+                    else{
+                        //sell
+                        int l = arr[i] + dp[i+1][1][k-1];
+
+                        //not sell
+                        int r = dp[i+1][0][k];
+                        profit = max(l, r);
+                    }
+                    dp[i][j][k] = profit;
+                }
+            }
+        }
+        return dp[0][1][k1];
     }
 };
