f5762eb39d30534694d13f2a09e56a451e8cf1e7
Sahil Lathiya <68152820+SahilLathiya@users.noreply.github.com>
2024-08-29 13:24:23 +0530

    Time: 7 ms (89.27%) | Memory: 8 MB (30.12%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -26,30 +26,30 @@ public:
         }
         return true;
     }
-    bool solve(vector<vector<char>>& board, int row, int col){
-        if(row==9)
-            return true;
-        if(col==9)
-            return solve(board, row+1, 0);
-        
-        // If the cell is already filled, move to the next cell
-        if (board[row][col] != '.') {
-            return solve(board, row, col + 1);
-        }
-        
-        for(int k=1;k<=9;k++){
-            if(isValid(board, row, col, k)){
-                board[row][col] = '0'+k;
-                if(solve(board, row, col+1))
-                    return true;
-                else{
-                    board[row][col] = '.';
+    bool solve(vector<vector<char>>& board){
+
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.'){
+
+                    for(int k=1;k<=9;k++){
+                        if(isValid(board, i, j, k)){
+                            board[i][j] = '0'+k;
+                            if(solve(board))
+                                return true;
+                            else{
+                                board[i][j] = '.';
+                            }
+                        }
+                    }
+                    return false;
+
                 }
             }
         }
-        return false;
+        return true;
     }
     void solveSudoku(vector<vector<char>>& board) {
-        cout<<solve(board, 0, 0);
+        cout<<solve(board);
     }
 };
