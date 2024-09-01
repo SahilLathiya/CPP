7c6e17bbb698fb4a575b566b0c4c4fad8e33526e
Sahil Lathiya <68152820+SahilLathiya@users.noreply.github.com>
2024-09-01 13:38:18 +0530

    Time: 11 ms (84.99%) | Memory: 11.3 MB (68.34%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,26 +1,80 @@
 class Solution {
 public:
-    bool isPalindrome(string &s, int l, int h){
-        while(l<h){
-            if(s[l]!=s[h])
-                return false;
-            l++;
-            h--;
-        }
-        return true;
-    }
     string longestPalindrome(string s) {
-        string ans;
-        int len = s.length();
-        for(int i=0;i<len;i++){
-            for(int j=i;j<len;j++){
-                if(isPalindrome(s,i,j)){
-                    if(j-i+1 > ans.length()){
-                        ans = s.substr(i, (j-i+1));
-                        // cout<<ans<<endl;
-                    }
+        if(s.size()<=1)
+            return s;
+        string ans = "";
+        int max_len = 0;
+        int n = s.size();
+        for(int i=1;i<n;i++){
+            int low = i;
+            int high = i;
+
+            while(s[low]==s[high]){
+                low--;
+                high++;
+
+                if(low==-1 || high==n)
+                    break;
+            }
+            if(low==-1){
+                int prev_ans_len = max_len;
+                max_len = max(max_len, high - (-1+1));
+                if(max_len>prev_ans_len){
+                    ans = s.substr(0, max_len);
                 }
             }
+            else if (high==n){
+                int prev_ans_len = max_len;
+                max_len = max(max_len, (high - (low+1)));
+                if(max_len>prev_ans_len){
+                    ans = s.substr(low+1, max_len);
+                }
+            }
+            else{
+                int prev_ans_len = max_len;
+                max_len = max(max_len, (high - (low+1)));
+                if(max_len>prev_ans_len){
+                    ans = s.substr(low+1, max_len);
+                }
+            }
+            // cout<<ans<<endl;
+
+
+            // For Even Length String
+            low = i-1;
+            high = i;
+
+            while(s[low]==s[high]){
+                low--;
+                high++;
+
+                if(low==-1 || high==n)
+                    break;
+            }
+            if(low==-1){
+                int prev_ans_len = max_len;
+                max_len = max(max_len, high - (-1+1));
+                if(max_len>prev_ans_len){
+                    ans = s.substr(0, max_len);
+                }
+            }
+            else if (high==n){
+                int prev_ans_len = max_len;
+                max_len = max(max_len, (high - (low+1)));
+                if(max_len>prev_ans_len){
+                    ans = s.substr(low+1, max_len);
+                }
+            }
+            else{
+                int prev_ans_len = max_len;
+                max_len = max(max_len, (high - (low+1)));
+                if(max_len>prev_ans_len){
+                    ans = s.substr(low+1, max_len);
+                }
+            }
+            // cout<<ans<<endl;
+
         }
         return ans;
     }
