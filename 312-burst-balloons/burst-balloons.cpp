e175029146ce6fed6c60439f51aaf42f4578a423
Sahil Lathiya <68152820+SahilLathiya@users.noreply.github.com>
2024-09-04 19:10:07 +0530

    Time: 209 ms (27.87%) | Memory: 12.4 MB (32.29%) - LeetSync

---
+++ 312-burst-balloons/burst-balloons.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int solve(vector<int>& arr, int i, int j, vector<vector<int>> &dp){
+        if(i>j)
+            return 0;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        int maxi = INT_MIN;
+        for(int ind=i;ind<=j;ind++){
+            int cost = arr[i-1] * arr[ind] * arr[j+1];
+            int l = solve(arr, i, ind-1, dp);
+            int r = solve(arr, ind+1, j, dp);
+            maxi = max(maxi, cost+l+r);
+        }
+        
+        return dp[i][j] = maxi;
+    }
+    int maxCoins(vector<int>& arr) {
+        int n = arr.size();
+        arr.insert(arr.begin(), 1);
+        arr.push_back(1);
+        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        int ans = solve(arr, 1, n, dp);
+        return ans;
+    }
+};
