653ade11189e3b76c3c0f83666a7c11d7162c653
Sahil Lathiya <68152820+SahilLathiya@users.noreply.github.com>
2024-09-28 13:01:16 +0530

    Time: 12 ms (61.79%) | Memory: 17.2 MB (79.75%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,23 +1,24 @@
+typedef pair<int, int> p;
 class Solution {
 public:
-    static bool cmp(pair<int, int>& a, pair<int, int>& b) {
-        return a.second > b.second;
-    }
-    vector<int> topKFrequent(vector<int>& arr, int k) {
-        map<int,int> mpp;
-        for(int i=0;i<arr.size();i++){
-            mpp[arr[i]]++;
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        
+        int n = nums.size();
+        unordered_map<int, int> mpp;
+        priority_queue<p, vector<p>, greater<p>> minH;
+        for(int i=0;i<n;i++){
+            mpp[nums[i]]++;
         }
-        priority_queue< pair<int, int>, vector<pair<int, int>>, greater<pair<int,int>> > minH;
-        for(auto it:mpp){   
-            minH.push({it.second, it.first});
+        for(auto i:mpp){
+            minH.push({i.second, i.first});
             if(minH.size()>k)
                 minH.pop();
         }
         vector<int> ans;
         while(!minH.empty()){
-            ans.push_back(minH.top().second);
+            p x = minH.top();
             minH.pop();
+            ans.push_back(x.second);
         }
         return ans;
     }
