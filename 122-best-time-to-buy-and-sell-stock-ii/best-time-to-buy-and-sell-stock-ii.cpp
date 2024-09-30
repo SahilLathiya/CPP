2479bcc9a07543cbc44e79ffd77aa9bad9ba0435
Sahil Lathiya <68152820+SahilLathiya@users.noreply.github.com>
2024-09-30 19:08:55 +0530

    Time: 0 ms (100.00%) | Memory: 16 MB (34.92%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -26,7 +26,30 @@ public:
     int maxProfit(vector<int>& arr) {
         // int ans = 0;
         int n = arr.size();
-        vector<vector<int>> dp(n, vector<int>(2, -1));
-        return solve(arr, 0, n, true, dp);
+        // vector<vector<int>> dp(n, vector<int>(2, -1));
+        // return solve(arr, 0, n, true, dp);
+
+        vector<int> prev(2, 0), temp(2, 0);
+        for(int i=n-1;i>=0;i--){
+            for(int canBuy=1;canBuy>=0;canBuy--){
+                if(canBuy){
+                    // Buy
+                    int l = -arr[i] + temp[false];
+
+                    // Not Buy
+                    int r = temp[canBuy];
+                    prev[canBuy] = max(l, r);
+                }
+                else {
+                    // Sale
+                    int l = arr[i] + temp[true];
+                    // Not Sale
+                    int r = temp[false];
+                    prev[canBuy] = max(l, r);
+                }
+            }
+            temp = prev;
+        }
+        return temp[true];
     }
 };
