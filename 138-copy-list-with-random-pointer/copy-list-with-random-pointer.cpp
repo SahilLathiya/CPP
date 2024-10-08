5ff758000a4f2dca0eb3f89b0a6da32b14d62032
Sahil Lathiya <68152820+SahilLathiya@users.noreply.github.com>
2024-10-08 19:54:24 +0530

    Time: 5 ms (64.87%) | Memory: 14.6 MB (65.58%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -17,24 +17,48 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
+        // copy each node
         Node * ptr = head;
-        Node * dummy = new Node(-1);
-        Node * ans = dummy;
-        unordered_map<Node*, Node*> mpp;
         while(ptr){
-            dummy->next = new Node(ptr->val);
-            mpp[ptr] = dummy->next;
+            Node * t = ptr->next;
+            ptr->next = new Node(ptr->val);
+            ptr->next->next = t;
+            ptr = ptr->next->next;
+        }
+        ptr = head;
+        while(ptr){
+            cout<<ptr->val<<' ';
             ptr = ptr->next;
-            dummy = dummy->next;
         }
 
+
+        // Copy Random Pointers
         ptr = head;
-        dummy = ans->next;
+        // cout<<endl;
         while(ptr){
-            dummy->random = mpp[ptr->random];
-            ptr = ptr->next;
-            dummy = dummy->next;
+            // cout<<ptr->val<<' ';
+            if(ptr->random)
+                ptr->next->random = ptr->random->next;
+            else
+                ptr->next->random = NULL;
+            ptr = ptr->next->next;
         }
-        return ans->next;
+        // ptr = head;
+        // while(ptr){
+        //     cout<<ptr->val<<' ';
+        //     ptr = ptr->next;
+        // }
+
+        // Seprate two LL
+        ptr = head;
+        Node * ans = new Node(0);
+        Node * t = ans;
+        while(ptr){
+            ans->next = ptr->next;
+            ptr->next = ptr->next->next;
+            ptr = ptr->next;
+            ans = ans->next;
+        }
+        return t->next;
     }
 };
