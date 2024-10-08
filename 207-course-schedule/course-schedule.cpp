51f62cc95f4c9c6594e88fd3def4e070420a899e
Sahil Lathiya <68152820+SahilLathiya@users.noreply.github.com>
2024-10-08 22:14:36 +0530

    Time: 14 ms (80.57%) | Memory: 18.5 MB (43.71%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -2,36 +2,36 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& arr) {
         int n = arr.size();
-
         vector<vector<int>> adj(numCourses);
-        vector<int> in_degree(numCourses, 0);
+        vector<int> inDegree(numCourses, 0);
+
         for(int i=0;i<n;i++){
             int course = arr[i][0];
             int prereq = arr[i][1];
             adj[prereq].push_back(course);
-            in_degree[course]++;
+            inDegree[course]++;
         }
-
+        
         queue<int> q;
         for(int i=0;i<numCourses;i++){
-            if(in_degree[i]==0){
+            if(!inDegree[i])
                 q.push(i);
-            }
         }
 
-        int completed_course = 0;
+        int completed_courses = 0;
         while(!q.empty()){
             int course = q.front();
             q.pop();
-            completed_course++;
+            
+            completed_courses++;
+            
             for(int i=0;i<adj[course].size();i++){
-                in_degree[adj[course][i]]--;
-                if(in_degree[adj[course][i]]==0){
+                inDegree[adj[course][i]]--;
+                if(inDegree[adj[course][i]]==0){
                     q.push(adj[course][i]);
                 }
             }
         }
-
-        return completed_course==numCourses;
+        return completed_courses==numCourses;
     }
 };
