418abfb572e1774fd98c27e01021e78d70712e86
Sahil Lathiya <68152820+SahilLathiya@users.noreply.github.com>
2024-10-08 22:24:24 +0530

    Time: 69 ms (17.82%) | Memory: 61.8 MB (27.02%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
-    int maxArea(vector<int>& height) {
-        int l = 0;
-        int r = height.size()-1;
+    int maxArea(vector<int>& arr) {
+        int l = 0, h = arr.size()-1;
         int ans = 0;
-        while(l<r){
-            int min_h = min(height[l], height[r]);
-            ans = max(ans, min_h * (r-l));
-            if(min_h==height[l])
+        while(l<h){
+            ans = max(ans, (h-l)*min(arr[l], arr[h]));
+            if(arr[l]<=arr[h])
                 l++;
             else
-                r--;
+                h--;
         }
         return ans;
     }
