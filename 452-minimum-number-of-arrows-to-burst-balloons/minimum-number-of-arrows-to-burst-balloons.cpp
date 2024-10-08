3b908d3e43a88f12f74c2f08184eab187017383c
Sahil Lathiya <68152820+SahilLathiya@users.noreply.github.com>
2024-10-08 17:14:01 +0530

    Time: 259 ms (46.45%) | Memory: 93.6 MB (73.14%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
-    int findMinArrowShots(vector<vector<int>>& points) {
-        if (points.empty()) {
-            return 0;
-        }
-        
-        // Sort the balloons by their end points (xend)
-        sort(points.begin(), points.end(), [](const vector<int>& a, const vector<int>& b) {
-            return a[1] < b[1];
-        });
-        
-        // Start with one arrow shot at the end of the first balloon
-        int arrows = 1;
-        int arrow_position = points[0][1];
-        
-        // Traverse through the balloons
-        for (int i = 1; i < points.size(); ++i) {
-            // If the start of the current balloon is beyond the arrow's position,
-            // we need a new arrow for this balloon
-            if (points[i][0] > arrow_position) {
-                ++arrows;
-                arrow_position = points[i][1];  // Move the arrow to the end of this balloon
+    static bool cmp(vector<int> &a, vector<int> &b){
+        return a[1]<b[1];
+    }
+    int findMinArrowShots(vector<vector<int>>& arr) {
+        int n = arr.size();
+        sort(arr.begin(), arr.end(), cmp);
+        int num_arrows = 1;
+        int arrow_pos = arr[0][1];
+        for(int i=1;i<n;i++){
+            if(arr[i][0]>arrow_pos){
+                num_arrows++;
+                arrow_pos = arr[i][1];
             }
         }
-        
-        return arrows;
+        return num_arrows;
     }
 };
