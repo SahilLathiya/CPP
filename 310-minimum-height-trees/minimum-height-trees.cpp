771b31950d0fde34c690dc5a70e27940476ce65c
Sahil Lathiya <68152820+SahilLathiya@users.noreply.github.com>
2024-10-09 11:45:06 +0530

    Time: 169 ms (15.08%) | Memory: 88.3 MB (7.31%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.cpp
+++ 310-minimum-height-trees/minimum-height-trees.cpp
@@ -1,39 +1,35 @@
 class Solution {
 public:
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
-        if (n == 1) return {0};  // A single node tree
+        if(n==1)
+            return {0};
         
-        // Build the graph
         vector<unordered_set<int>> graph(n);
-        for (const auto& edge : edges) {
-            graph[edge[0]].insert(edge[1]);
-            graph[edge[1]].insert(edge[0]);
+        for(auto e:edges){
+            graph[e[0]].insert(e[1]);
+            graph[e[1]].insert(e[0]);
         }
-        
-        // Initialize the first layer of leaves
+
         vector<int> leaves;
-        for (int i = 0; i < n; ++i) {
-            if (graph[i].size() == 1) {
+        for(int i=0;i<graph.size();i++){
+            if(graph[i].size()==1)
                 leaves.push_back(i);
-            }
         }
-        
-        // Trim the leaves layer by layer
-        int remainingNodes = n;
-        while (remainingNodes > 2) {
-            remainingNodes -= leaves.size();
+
+        int totalNodes = n;
+        while(totalNodes>2){
+            totalNodes -= leaves.size();
             vector<int> newLeaves;
-            for (int leaf : leaves) {
-                int neighbor = *graph[leaf].begin();
-                graph[neighbor].erase(leaf);
-                if (graph[neighbor].size() == 1) {
+            for(auto l:leaves){
+                int neighbor = *graph[l].begin();
+                graph[neighbor].erase(l);
+                if(graph[neighbor].size()==1){
                     newLeaves.push_back(neighbor);
                 }
             }
             leaves = newLeaves;
         }
         
-        // The remaining nodes are the MHT roots
         return leaves;
     }
 };
